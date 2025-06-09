420b9c6ba19402ff249d4b2a5b331911084b5d68
Godwin Mayodi <godwin.mayodi@student.moringaschool.com>
2025-06-09 13:36:08 +0300

    feat: add Programs and Title components with styling; enhance Hero component layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Hero from "./components/Hero/Hero.jsx";
+import Programs from "./components/Programs/Programs.jsx";
+import Title from "./components/Title/Title.jsx";
 
 const App = () => {
     return (
         <div>
             <Navbar/>
             <Hero/>
+            <div className='container'>
+                <Title subtitle='OUR PROGRAMS' title='What We Offer'/>
+                <Programs/>
+            </div>
+
         </div>
     );
 };
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Hero.css';
+import dark_arrow from '../../assets/dark-arrow.png';
 
 const Hero = () => {
     return (
@@ -10,13 +11,11 @@ const Hero = () => {
                 </h1>
                 <p>
                     Our cutting edge curriculum is designed to empower students with yhe knowlwdge, skills, and experiences needed to excel in the dynamic field of education
-                <button className='btn'>
-                    Explore more
-                </button>
                 </p>
-
+                <button className='btn'>
+                    Explore more <img src={dark_arrow} alt=''/>
+                </button>
             </div>
-
         </div>
     );
 };
